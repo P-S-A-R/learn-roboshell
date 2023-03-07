c5f2a9590aaf390da1103af45cf833b750b8322c
Satya Avinash Reddy Pamireddy <reddyavinash61@gmail.com>
2023-03-07 17:56:36 +0100

    updating ./learn-roboshell

--- frontend.sh
+++ frontend.sh
@@ -7,9 +7,6 @@ curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.z
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 echo -e "\e[32mCopying nginx\e[0m"
-pwd
-ls -l
-
 cp configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
 echo -e "\e[34mEnabling nginx\e[0m"
 systemctl enable nginx
