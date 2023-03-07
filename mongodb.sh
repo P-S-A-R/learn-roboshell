8e2facbf70bdf37482c95e11a9bc2204857a01b7
Satya Avinash Reddy Pamireddy <reddyavinash61@gmail.com>
2023-03-07 23:17:58 +0100

    updating ./learn-roboshell

--- mongodb.sh
+++ mongodb.sh
@@ -1,7 +1,9 @@
+echo -e "\e[32mCopying mongodb\e[0m"
+cp configs/mongodb.repo /etc/yum.repos.d/mongo.repo
 echo -e "\e[35mInstlling mongodb\e[0m"
 yum install mongodb-org -y
-echo -e "\e[35mEnbling mongodb\e[0m"
+echo -e "\e[34mEnbling mongodb\e[0m"
 systemctl enable mongod
-echo -e "\e[35mStarting mongodb\e[0m"
+echo -e "\e[33mStarting mongodb\e[0m"
 systemctl start mongod
-systemctl restart mongod
+
