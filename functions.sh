d16e74f1d362eb946a7815b6e7b5fc4cda8ab8d8
Satya Avinash Reddy Pamireddy <reddyavinash61@gmail.com>
2023-03-10 17:31:57 +0100

    updating ./learn-roboshell

---
+++ functions.sh
@@ -0,0 +1,14 @@
+xyz()
+{
+  echo first argument=$1
+  echo second argument=$2
+  echo all arguments=$*
+  echo no of arguments=$#
+  echo $a
+  b=200
+
+}
+
+xyz 123 456
+a=120
+echo value of b =$b
