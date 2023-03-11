d37de61c9efe711e0ab769476aa50532a393fab7
Satya Avinash Reddy Pamireddy <reddyavinash61@gmail.com>
2023-03-11 02:14:26 +0100

    updating ./learn-roboshell

--- catalogue.sh
+++ catalogue.sh
@@ -1,31 +1,31 @@
 source common.sh
 print_head "Looking for Catalogue"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>{log_file}
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log_file}
 print_head "Installing Catalogue"
-yum install nodejs -y &>>{log_file}
+yum install nodejs -y &>>${log_file}
 print_head "Adding user"
-useradd roboshop &>>{log_file}
+useradd roboshop &>>${log_file}
 print_head "Creating app directory"
-mkdir /app &>>{log_file}
+mkdir /app &>>${log_file}
 print_head "Removing content in app"
-rm -rf /app/* &>>{log_file}
+rm -rf /app/* &>>${log_file}
 print_head "Downloading Catalogue"
-curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>{log_file}
+curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>${log_file}
 cd /app
 print_head "Extracting Catalogue"
-unzip /tmp/catalogue.zip &>>{log_file}
+unzip /tmp/catalogue.zip &>>${log_file}
 cd /app
 print_head "Reinstalling  Catalogue"
-npm install &>>{log_file}
-cp ${code_dir}/configs/catalogue.service /etc/systemd/system/catalogue.service &>>{log_file}
+npm install &>>${log_file}
+cp ${code_dir}/configs/catalogue.service /etc/systemd/system/catalogue.service &>>${log_file}
 print_head "Reloading Catalogue"
 systemctl daemon-reload
 print_head "Enabling Catalogue"
 systemctl enable catalogue
 print_head "Starting Catalogue"
 systemctl start catalogue
-cp ${code_dir}/configs/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>{log_file}
+cp ${code_dir}/configs/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>${log_file}
 print_head "Installing mongodb"
 yum install mongodb-org-shell -y &>>{log_file}
 print_head "Downloading schema"
-mongo --host mongodb.devopsb71.shop </app/schema/catalogue.js &>>{log_file}
+mongo --host mongodb.devopsb71.shop </app/schema/catalogue.js &>>${log_file}
