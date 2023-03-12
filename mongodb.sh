98f590247ec6eae8870ec69a5101cd8668fb8c71
Satya Avinash Reddy Pamireddy <reddyavinash61@gmail.com>
2023-03-12 23:36:09 +0100

    updating ./learn-roboshell

--- mongodb.sh
+++ mongodb.sh
@@ -5,8 +5,8 @@ print_head "Instlling mongodb"
 yum install mongodb-org -y &>>${log_file}
 print_head "updating mongo db ip address"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf  &>>${log_file}
-print_head "Enabling mongodb"  &>>${log_file}
-systemctl enable mongod
-print_head "starting mongodb"  &>>${log_file}
-systemctl restart mongod
+print_head "Enabling mongodb"
+systemctl enable mongod &>>${log_file}
+print_head "starting mongodb"
+systemctl restart mongod &>>${log_file}
 
