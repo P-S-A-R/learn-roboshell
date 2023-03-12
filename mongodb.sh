1af97b1b066954371567a95be529fe403aa5a691
Satya Avinash Reddy Pamireddy <reddyavinash61@gmail.com>
2023-03-12 23:31:25 +0100

    updating ./learn-roboshell

--- mongodb.sh
+++ mongodb.sh
@@ -8,5 +8,5 @@ sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf  &>>${log_file}
 print_head "Enabling mongodb"  &>>${log_file}
 systemctl enable mongod
 print_head "starting mongodb"  &>>${log_file}
-systemctl start mongod
+systemctl restart mongod
 
