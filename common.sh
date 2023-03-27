f97559935a1418f63ed73f3954c9148871e70752
Satya Avinash Reddy Pamireddy <reddyavinash61@gmail.com>
2023-03-27 19:30:25 +0200

    updating ./learn-roboshell

--- common.sh
+++ common.sh
@@ -21,6 +21,8 @@ systemd_setup() {
   cp ${code_dir}/configs/${component}.service /etc/systemd/system/${component}.service &>>${log_file}
   status_check $?
 
+  sed -i -e "s/ROBOSHOP_APP_PASSWORD/${roboshop_app_pssword}/" /etc/systemd/system/${component}.service &>>${log_file}
+
   print_head "Relaoding user"
   systemctl daemon-reload  &>>${log_file}
   status_check $?
@@ -119,6 +121,22 @@ java() {
  print_head "Downloading dependencies and packages"
  mvn clean package &>>${log_file}
  mv target/${component}-1.0.jar ${component}.jar &>>${log_file}
+ status_check $?
+
+ schema_setup
+ systemd_setup
+}
+
+python() {
+
+  print_head "install python"
+  yum install python36 gcc python3-devel -y &>>${log_file}
+  status_check $?
+
+ app_prereq_setup
+
+ print_head "Downloading dependencies"
+ pip3.6 install -r requirements.txt &>>${log_file}
  status_check $?
 
  schema_setup
