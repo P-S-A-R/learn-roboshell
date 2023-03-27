72f9521dd5099bf9e2cc851016193a1ef05798c0
Satya Avinash Reddy Pamireddy <reddyavinash61@gmail.com>
2023-03-27 17:18:02 +0200

    updating ./learn-roboshell

--- mysql.sh
+++ mysql.sh
@@ -27,7 +27,11 @@ systemctl start mysqld &>>${log_file}
 status_check $?
 
 print_head "set password"
-mysql_secure_installation --set-root-pass ${mysql_root_password} &>>${log_file}
+echo show databses | mysql -uroot -p${mysql_root_password}  &>>${log_file}
+if [ $? -ne 0 ]; then
+  mysql_secure_installation --set-root-pass ${mysql_root_password} &>>${log_file}
+fi
+
 status_check $?
 
 
