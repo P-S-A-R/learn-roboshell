0753dcab5fd6982ae1cd4651550d0db4e0c7c1bf
Satya Avinash Reddy Pamireddy <reddyavinash61@gmail.com>
2023-03-29 16:24:10 +0200

    updating ./learn-roboshell

--- dispatch.sh
+++ dispatch.sh
@@ -1,17 +1,6 @@
 source common.sh
 
-mysql_root_password=$1
-
-if [ -z "${mysql_root_password}" ]; then
-  echo -e "\e[31mMissing mysql root password argument\e[0m"
-  exit 1
-fi
-print_head "installing golang"
-yum install golang -y
-status_check $?
-
-print_head "cleaning packages"
-
+print_head "clean packages"
 yum clean packages
 status_check $?
 
